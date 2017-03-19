581d2b02bab6efe30f2548cf60ce3232c49ef107
Jack Hughes <jhughes@jackwebdev.com>
2017-03-19 17:26:41 -0400

    FIX - File Path echo

--- routes/route-family.js
+++ routes/route-family.js
@@ -4,10 +4,12 @@ var fs = require('fs')
 var path = require('path');
 var appDir = path.dirname(require.main.filename);
 
-const family_file = "family.json"
+const familyFile = "family.json"
+const familyPath = (appDir + '/data/' + familyFile)
 
 // define the home page route
 router.get('/', function (req, res) {
+    console.log(familyPath)
     res.json(readFamily())
 })
 // define the about route
@@ -16,7 +18,7 @@ router.get('/about', function (req, res) {
 })
 
 function readFamily() {
-    fs.readFile((appDir + '/data/' + family_file), 'utf8', function readFileCallback(err, data){
+    fs.readFile(familyPath, 'utf8', function readFileCallback(err, data){
         if (err){
             console.log(err)
         } else {
