872eb636abd4b54439b1bc00698ff5fcf0eb92ab
Jack Hughes <jhughes@jackwebdev.com>
2017-03-19 16:50:48 -0400

    Express server updates (routes)

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-const express = require('express');
-const app = express();
-const port = 3000;
+const express = require('express')
+const app = express()
+const port = 3000
+
+var route_main = require('./routes/route-main')
 
 app.use((err, request, response, next) => {  
     // log the error, for now just console.log
     console.log(err);
-    response.status(500).send('Something broke!');
+    response.status(500).send('Something broke!')
 });
 
-app.get('/', function (req, res) {
-    res.send('Hello World!')
-});
+app.all('/', function (req, res, next) {
+    console.log(req);
+  next() // pass control to the next handler
+})
 
-app.listen(port);
+app.use('/', route_main)
+
+app.listen(port)
