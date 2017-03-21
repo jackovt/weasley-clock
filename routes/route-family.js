61111f81cd5b285e591a68b1b118448b090867b9
Jack Hughes <jhughes@jackwebdev.com>
2017-03-21 19:00:26 -0400

    Adding Location

--- routes/route-family.js
+++ routes/route-family.js
@@ -20,41 +20,74 @@ router.get('/:familyMemberId', function (req, res) {
     readFamily(res, familyMemberId)
 })
 
-// define the about route
-router.get('/about', function (req, res) {
-  res.send('About Page')
+// define the home page route
+router.get('/location/:locationId', function (req, res) {
+    var locationId = req.params.locationId
+    console.log(locationId)
+    readFamilyByLocation(res, locationId)
 })
 
-function readFamily(res, familyMemberId) {
+function readFamilyJson(callback) {
     fs.readFile(familyPath, 'utf8', function readFileCallback(err, data){
         if (err){
             console.log(err)
             throw new Error("can't read file")
         } else {
             obj = JSON.parse(data)
-            if (obj) {
-                if (!familyMemberId) {
-                    console.log(obj)
-                    res.json(obj)
-                } else {
-                    if (obj.members && obj.members.length && obj.members.length > 0) {
-                        var familyMembers = obj.members
-                        for (index = 0; index < familyMembers.length; ++index) {
-                            var familyMember = familyMembers[index]
-                            if (familyMember.id === familyMemberId) {
-                                console.log(familyMember)
-                                res.json(familyMember)
-                                return;
-                            }
+            callback(obj)
+        }
+    });
+}
+
+function readFamily(res, familyMemberId) {
+    readFamily(function(obj) {
+        if (obj) {
+            if (!familyMemberId) {
+                console.log(obj)
+                res.json(obj)
+            } else {
+                if (obj.members && obj.members.length && obj.members.length > 0) {
+                    var familyMembers = obj.members
+                    for (index = 0; index < familyMembers.length; ++index) {
+                        var familyMember = familyMembers[index]
+                        if (familyMember.id === familyMemberId) {
+                            console.log(familyMember)
+                            res.json(familyMember)
+                            return;
                         }
                     }
-                    throw new Error("can't find family member")
-                    return;
                 }
-            } else {
-                console.log("family obj was null")
-                throw new Error("can't get family")
+                throw new Error("can't find family member")
+                return;
             }
+        } else {
+            console.log("family obj was null")
+            throw new Error("can't get family")
+        }
+    });
+}
+
+function readFamilyByLocation(res, locationId) {
+    readFamilyJson(function(obj) {
+        if (obj) {
+            if (locationId) {
+                if (obj.members && obj.members.length && obj.members.length > 0) {
+                    var familyMembers = obj.members
+                    for (index = 0; index < familyMembers.length; ++index) {
+                        var familyMember = familyMembers[index]
+                        if (familyMember.location === familyMemberId) {
+                            console.log(familyMember)
+                            res.json(familyMember)
+                            return;
+                        }
+                    }
+                }
+                throw new Error("can't find family member")
+                return;
+            }
+        } else {
+            console.log("family obj was null")
+            throw new Error("can't get family")
         }
     });
 }
