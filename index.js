e88747ea5c8c4b06f5bbbff05375c41f617edcc0
Jack Hughes <jhughes@jackwebdev.com>
2017-03-23 17:26:08 -0400

    Adding POST call to update family member's location - fix 7

--- index.js
+++ index.js
@@ -7,19 +7,13 @@ var route_main = require('./routes/route-main')
 var route_family = require('./routes/route-family')
 var route_locations = require('./routes/route-locations')
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use((err, request, response, next) => {  
     // log the error, for now just console.log
     console.log(err);
     response.status(500).send('Something broke!')
-});
-
-app.all('/', function (req, res, next) {
-  next() // pass control to the next handler
 })
 
 app.use('/', route_main)
